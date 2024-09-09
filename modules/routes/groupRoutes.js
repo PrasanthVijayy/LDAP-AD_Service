030a2f6b9fbf9a9d771b87a2ce79a86ad9b4320a
Prasanth <vkprasanth06@gmail.com>
2024-09-09 22:37:51 +0530

    Update: working on groupOperations API

--- modules/routes/groupRoutes.js
+++ modules/routes/groupRoutes.js
@@ -1,14 +1,17 @@
 import express from 'express';
-import { 
-    listGroups, 
-    addToGroup, 
-    deleteFromGroup 
-} from '../controllers/groupController.js';
+import GroupController from "../controllers/groupController.js";
 
-const router = express.Router();
+const groupController = new GroupController();
 
-router.get('/listgroups', listGroups);
-router.post('/addtogroup', addToGroup);
-router.post('/deletefromgroup', deleteFromGroup);
+const groupRoutes = (app) => {
+    const router = express.Router();
+    app.use("/LDAP/v1/groups", router);
 
+    router.post("/createGroup", (req, res, next) => groupController.createGroup(req, res, next));
+    router.get("/listGroups", (req, res, next) => groupController.listGroups(req, res, next));
+    router.post("/addToGroup", (req, res, next) => groupController.addToGroup(req, res, next));
+    router.post("/deleteFromGroup", (req, res, next) => groupController.deleteFromGroup(req, res, next));
+    router.post("/addAdminGroup", (req, res, next) => groupController.addAdminGroup(req, res, next));
+    router.post("/deleteAdminGroup", (req, res, next) => groupController.deleteAdminGroup(req, res, next));
+}
 export default router;
