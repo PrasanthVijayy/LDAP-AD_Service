1abf68699d34d6ac3433c84f7bda0a26a856f620
prasanthVijayy <vkprasanth06@gmail.com>
2024-09-24 18:12:19 +0530

    added SHA1 hashing method

---
+++ utils/encryption.js
@@ -0,0 +1,21 @@
+import crypto from "crypto";
+
+function createSSHAHash(password) {
+  // Generate a 4-byte salt
+  const salt = crypto.randomBytes(4);
+  
+  // Create SHA1 hash
+  const hash = crypto.createHash('sha1');
+  hash.update(password);
+  hash.update(salt);
+  const digest = hash.digest();
+  
+  // Concatenate the hash and salt
+  const sshaHash = Buffer.concat([digest, salt]);
+  
+  // Encode in base64 and format with SSHA prefix
+  return `{SSHA}${sshaHash.toString('base64')}`;
+}
+
+
+export { createSSHAHash };
