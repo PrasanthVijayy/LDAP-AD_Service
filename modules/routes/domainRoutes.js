799f438c9eda74a641550a0b5cd6f92d5af147d1
prasanthVijayy <vkprasanth06@gmail.com>
2024-11-04 13:10:56 +0530

    conflict issue

--- modules/routes/domainRoutes.js
+++ modules/routes/domainRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import DomainController from "../controllers/domainController.js";
 import apiLimiter from "../../middleware/apiLimiter.js";
+import { sessionMiddleware } from "../../middleware/sessionMiddleware.js";
 
 const domainController = new DomainController();
 
@@ -8,7 +9,7 @@ const domainRoutes = (app) => {
   const router = express.Router();
   app.use("/LDAP/v1/dc", router);
 
-  router.get("/listDCs", apiLimiter(10), domainController.listDCs); // list DCs
+  router.get("/listDCs", sessionMiddleware, apiLimiter(10), domainController.listDCs); // list DCs
 };
 
 export default domainRoutes;
