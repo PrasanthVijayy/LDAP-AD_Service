ffceaee48f08893b63956ddcf104f148b40fd6f3
prasanthVijayy <vkprasanth06@gmail.com>
2025-01-08 12:06:17 +0530

    Refactor SAML configuration to use environment variables for sensitive URLs

--- utils/samlUtils.js
+++ utils/samlUtils.js
@@ -1,13 +1,15 @@
-import fs from "fs";
 "use strict";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const samlUtils = {
-  entryPoint: "https://adfs.cybernexa.com/adfs/ls/idpinitiatedsignonpage.aspx",
-  issuer: "https://192.168.0.145/",
-  callbackUrl: "https://192.168.0.145/login/callback",
+  entryPoint: `${process.env.SAML_ENTRY}`,
+  issuer: `${process.env.APP_LOGIN_URL}`,
+  callbackUrl: `${process.env.SAML_CALLBACK}`,
   idpCert: fs.readFileSync("Certificates/adCert_ip144.pem", "utf-8"),
   identifierFormat: null,
-  logoutURL: "https://adfs.cybernexa.com/adfs/ls/?wa=wsignout1.0",
+  logoutURL: `${process.env.SAML_LOGOUT}`,
 };
 
 /*  ---------- SAML CONFIG - TESTING START ---------- */
@@ -21,15 +23,9 @@ export const samlUtils = {
 // };
 /*  ---------- SAML CONFIG - TESTING END ---------- */
 
-// https://sso.cybernexa.com/adfs/ls/idpinitiatedsignonpage.aspx
-//   callbackUrl: "https://remote.cybernexa.com/login/callback",
-
-// const cert = fs.readFileSync("./certificate/cert3.pem", "utf-8");
-// console.log(cert); // Ensure the certificate is correctly loaded
-
 // Checking the signature algorithm ⚠️
 // openssl x509 -in idp-certificate.pem -text -noout
 
 // Convert file format from crt to pem ⚠️
 // openssl x509 -in cybernexa.crt -out cybernexa1.pem -outform PEM
-// openssl x509 -inform DER -in certificate.crt -outform PEM -out certificate.pem 
+// openssl x509 -inform DER -in certificate.crt -outform PEM -out certificate.pem
